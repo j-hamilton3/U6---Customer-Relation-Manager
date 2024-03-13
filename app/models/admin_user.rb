6b292c4a990f4e6b3796ac2cc6deaa9b768b0bab
j-hamilton3 <jhamilton3@rrc.ca>
2024-03-12 19:55:48 -0500

    Added image to model.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,11 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    excluded_attributes = ['id', 'encrypted_password', 'password_reset_token', 'reset_password_sent_at', 'remember_created_at', 'sign_in_count', 'current_sign_in_at', 'last_sign_in_at', 'current_sign_in_ip', 'last_sign_in_ip', 'confirmation_token', 'confirmed_at', 'confirmation_sent_at', 'unconfirmed_email', 'failed_attempts', 'unlock_token', 'locked_at', 'owner']
+    (column_names - excluded_attributes).sort
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,12 @@
 class Customer < ApplicationRecord
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "full_name", "id", "id_value", "notes", "phone_number", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["image_attachment", "image_blob"]
+  end
+
+  has_one_attached :image
 end
