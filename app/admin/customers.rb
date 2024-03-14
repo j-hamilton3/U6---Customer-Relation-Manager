752045aad73bc1a5d1a8d08eb4c468bf0cd76039
j-hamilton3 <jhamilton3@rrc.ca>
2024-03-14 14:49:24 -0500

    Added images to activeadmin.

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -15,4 +15,13 @@ ActiveAdmin.register Customer do
   #   permitted
   # end
 
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image, size:"200x200") : ""
+    end
+    f.actions
+  end
+
 end
